dc876277e124de2e3e7320076c9a9e8a8a9735c3
Guilherme-Gomes-Luccas <guilhermegomes.luccas@gmail.com>
2025-04-22 19:34:35 -0300

    fix: max upload size

--- routes/fileRoute.ts
+++ routes/fileRoute.ts
@@ -5,7 +5,10 @@ import { uploadAudio } from '../controllers/fileController';
 const router = express.Router();
 
 // Armazena os arquivos temporariamente em /tmp/uploads (Render permite escrita nessa pasta)
-const upload = multer({ dest: '/tmp/uploads' });
+const upload = multer({
+    dest: '/tmp/uploads',
+    limits: { fileSize: 50 * 1024 * 1024 }, // 50MB
+});
 
 router.post('/upload', upload.single('file'), uploadAudio);
 
